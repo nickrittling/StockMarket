7718051df68706dca7abdf57be0515d5fee13e2a
OVUSA <velichko.olga.chicago@gmail.com>
2022-12-09 14:09:39 -0500

    added limit order button & function

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -18,26 +18,27 @@ namespace Stock_Market
         }
         protected void GridView1_RowCommand1(object sender, GridViewCommandEventArgs e)
         {
+
+            // Convert the row index stored in the CommandArgument
+            // property to an Integer.
+            int index = Convert.ToInt32(e.CommandArgument);
+
+
+            GridViewRow selectedRow = GridView1.Rows[index];
+            TableCell stockName = selectedRow.Cells[0];
+            string currectStock = stockName.Text;
+
+            SqlConnections.CurrentStockSymbol = currectStock;
             // If multiple ButtonField column fields are used, use the
             // CommandName property to determine which button was clicked.
             if (e.CommandName == "Trade")
             {
 
-                // Convert the row index stored in the CommandArgument
-                // property to an Integer.
-                int index = Convert.ToInt32(e.CommandArgument);
-
-                
-                GridViewRow selectedRow = GridView1.Rows[index];
-                TableCell stockName = selectedRow.Cells[0];
-                string currectStock= stockName.Text;
-
-                SqlConnections.CurrentStockSymbol = currectStock;
-
                 Response.Redirect("/Pages/SelectedStock");
-
-
-
+            }
+            else
+            {
+                Response.Redirect("/Pages/SetUpLimit");
             }
 
         }

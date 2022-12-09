b1d220b97f4d9c9eaf5f0c18bdded2f7b4057651
OVUSA <velichko.olga.chicago@gmail.com>
2022-12-08 21:54:25 -0500

    added dynamic ID function

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Stock_Market.DB;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 
@@ -7,21 +9,34 @@ namespace Stock_Market.Models
 {
     public class Transaction
     {
-        public int Id { get; set; }
+        public int Id { get; }
         public int UserID { get; set; }
         public int StockID { get; set; }
         public int StockAmount { get;set; }
         public double TransactionPrice { get; set; }
         public string dateTime { get; set; }
-        public Transaction(int id, int userID, int stockID, int stockAmount, double transactionPrice, string dateTime)
+        public string Trade { get; set; }
+        public Transaction( int userID, int stockID, int stockAmount, double transactionPrice, string dateTime, string trade)
         {
-            Id = id;
+            Id = getID();
             UserID = userID;
             StockID = stockID;
             StockAmount = stockAmount;
             TransactionPrice = transactionPrice;
             this.dateTime = dateTime;
+            this.Trade = trade;  
         }
+
+        public int getID()
+        {
+            SqlConnections sc = new SqlConnections();
+            string sql = "Select COUNT(*) from Transactions;";
+            DataSet ds = sc.ExecuteSelect(sql);
+            int Id = (int)ds.Tables[0].Rows[0][0] + 1;
+            return Id;
+        }
+
+        
 
     }
 }

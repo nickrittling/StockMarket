dc5cfb577a0a9a044b81e222a7911e766ed7c5c5
Nick Rittling <nickrittling@yahoo.com>
2022-12-10 17:34:20 -0600

    signup/login

--- Account/Login.aspx.cs
+++ Account/Login.aspx.cs
@@ -1,60 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
 using System.Web;
 using System.Web.UI;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.Owin;
-using Owin;
-using Stock_Market.Models;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+
 
 namespace Stock_Market.Account
 {
-    public partial class Login : Page
+    public partial class Login : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            RegisterHyperLink.NavigateUrl = "Register";
-            // Enable this once you have account confirmation enabled for password reset functionality
-            //ForgotPasswordHyperLink.NavigateUrl = "Forgot";
-            OpenAuthLogin.ReturnUrl = Request.QueryString["ReturnUrl"];
-            var returnUrl = HttpUtility.UrlEncode(Request.QueryString["ReturnUrl"]);
-            if (!String.IsNullOrEmpty(returnUrl))
-            {
-                RegisterHyperLink.NavigateUrl += "?ReturnUrl=" + returnUrl;
-            }
+
         }
 
-        protected void LogIn(object sender, EventArgs e)
+        protected void Button1_Click(object sender, EventArgs e)
         {
-            if (IsValid)
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from Users where EMAIL =@EMAIL and PASSWORD=@PASSWORD", con);
+            cmd.Parameters.AddWithValue("@EMAIL", TextBox1.Text);
+            cmd.Parameters.AddWithValue("@PASSWORD", TextBox2.Text);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            if (dt.Rows.Count > 0)
             {
-                // Validate the user password
-                var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
-                var signinManager = Context.GetOwinContext().GetUserManager<ApplicationSignInManager>();
+                HttpCookie userInfo = new HttpCookie("userInfo");
+                userInfo["UserID"] = "Annathurai";
+                userInfo["UserColor"] = "Black";
+                userInfo.Expires.Add(new TimeSpan(0, 1, 0));
+                Response.Cookies.Add(userInfo);
+                Response.Redirect("~/Default.aspx");
 
-                // This doen't count login failures towards account lockout
-                // To enable password failures to trigger lockout, change to shouldLockout: true
-                var result = signinManager.PasswordSignIn(Email.Text, Password.Text, RememberMe.Checked, shouldLockout: false);
+            }
 
-                switch (result)
-                {
-                    case SignInStatus.Success:
-                        IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
-                        break;
-                    case SignInStatus.LockedOut:
-                        Response.Redirect("/Account/Lockout");
-                        break;
-                    case SignInStatus.RequiresVerification:
-                        Response.Redirect(String.Format("/Account/TwoFactorAuthenticationSignIn?ReturnUrl={0}&RememberMe={1}", 
-                                                        Request.QueryString["ReturnUrl"],
-                                                        RememberMe.Checked),
-                                          true);
-                        break;
-                    case SignInStatus.Failure:
-                    default:
-                        FailureText.Text = "Invalid login attempt";
-                        ErrorMessage.Visible = true;
-                        break;
-                }
+            else
+            {
+
+                Label3.Visible = true;
+                Label3.Text = "Wrong Details";
             }
         }
     }
